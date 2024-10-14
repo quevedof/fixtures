27f39648c31551461e54660b2cf633d6c9eec3a0
Freddy <freddyque96@gmail.com>
2024-10-14 16:09:36 +0100

    fix: number of fixtures

--- core/football.py
+++ core/football.py
@@ -2,11 +2,12 @@ import requests
 from django.core.cache import cache
 from .utils import get_date, check_session_list, check_cache, cache_timeout
 class FootballFixture():
-    def __init__(self, club_name, venue, opponent, timestamp):
+    def __init__(self, club_name, venue, opponent, timestamp, tournament):
         self.club_name = club_name
         self.venue = venue
         self.opponent = opponent
         self.date = get_date(timestamp)
+        self.tournament = tournament
 
 # returns the next 4 fixtures of the given football club 
 def get_football_fixtures(name, session_list):
@@ -25,15 +26,18 @@ def get_football_fixtures(name, session_list):
         # if fixtures are in cache, get them
         in_cache = check_cache(cached_data, club_name, all_fixtures)
 
-        if(not in_cache):
+        if not in_cache:
             # get fixtures using the club id
             fixtures_url = f"https://allsportsapi2.p.rapidapi.com/api/team/{club_id}/matches/next/0"
             fixtures_response = requests.get(fixtures_url, headers=headers).json()
-            print('api call for football fix')
-            # list of club fixtures
+            print('api call for football fixture')
+            # list of fixtures
             fixtures = []
-            # get the next 4 fixtures
-            for i in range(4):
+
+            # number of fixtures
+            fixtures_num = 4 if len(fixtures_response["events"]) > 3 else len(fixtures_response["events"])
+            # get the next fixtures
+            for i in range(fixtures_num):
                 # home or away
                 venue = "Home" if fixtures_response["events"][i]["homeTeam"]["name"] == club_name else "Away"
 
@@ -42,8 +46,11 @@ def get_football_fixtures(name, session_list):
 
                 # timestamp
                 timestamp = fixtures_response["events"][i]["startTimestamp"]
+
+                # tournament
+                tournament = fixtures_response["events"][i]["tournament"]["name"]
                 
-                fixture = FootballFixture(club_name, venue, opponent, timestamp)
+                fixture = FootballFixture(club_name, venue, opponent, timestamp, tournament)
                 fixtures.append(fixture)
 
             all_fixtures.append(fixtures)
